63cec935b520cee92d80b7a4207bb153a4f1e035
akashkoliak14 <akashkoli100@gmail.com>
2022-03-26 00:19:53 +0530

    "sch"

--- src/component/Schedule.jsx
+++ src/component/Schedule.jsx
@@ -2,21 +2,34 @@ import React from 'react'
 
 const Schedule = (props) => {
   return (
-    <div><h1>Schedule</h1>
+    <div>
        
-       <input type="text" placeholder='Add category' onChange={props.itemEvent} />
+       <div >
+     
+  <div className='main_div'>
+  
+        <div className='madhala_card'>
+        <h1>
+          TODAYS CATEGORY
+        </h1>
         
-        <select onChange={props.iEvent}
-        type="select" >
-          <option>  Personal</option>
-          <option>  Shoping</option>
-          <option>  Sports</option>
-          <option>  Events</option>
-          <option>  Works</option>
-          </select>  
-          
-          <button onClick={props.listOfItems}>+</button>   
-    </div>
+       
+        <div>
+       
+        
+
+
+
+       
+       
+     </div> 
+    
+        </div> 
+
+        </div>
+  </div>
+    </div> 
+    
   )
 }
 
